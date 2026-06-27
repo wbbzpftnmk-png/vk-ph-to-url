ea0c5ab7fd6b93a65083947c753e66d550a75189
Zakharov Maksim <wbbzpftnmk@privaterelay.appleid.com>
2026-06-28 02:59:22 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ APP = ['main.py']
 OPTIONS = {
     'argv_emulation': True,
     'packages': ['flask', 'requests', 'webview'],
-    'includes': ['WebKit', 'Foundation'],
     'iconfile': 'icon.icns',
     'plist': {
         'CFBundleName': 'vk ph to url',
         'CFBundleDisplayName': 'vk ph to url',
-        'CFBundleIdentifier': 'com.vkalbum.photourls',
+        'CFBundleIdentifier': 'com.maksimzaharov.vkph2url',
         'CFBundleShortVersionString': '1.0.0',
         'CFBundleVersion': '1.0.0',
         'LSMinimumSystemVersion': '12.0',
